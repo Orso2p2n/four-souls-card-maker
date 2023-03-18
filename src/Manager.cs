17e3c48a9f78e141257cff8722360aa3f4887ac9
Orsopidou <Orsoph@gmail.com>
2023-03-18 17:03:57 +0100

    Godot initial setup

---
+++ src/Manager.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Manager : Node
+{
+    public override void _Ready() {
+        base._Ready();
+    }
+
+    public override void _Process(double delta) {
+        base._Process(delta);
+    }
+}
