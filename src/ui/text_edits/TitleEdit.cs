e465253316722efe805e600451f52e8c5d889f25
Orsopidou <Orsoph@gmail.com>
2023-04-09 10:01:10 +0200

    Save WIP - Title and credits

---
+++ src/ui/text_edits/TitleEdit.cs
@@ -0,0 +1,24 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class TitleEdit : LineEdit
+{
+	public void OnTextChanged(string text) {
+		Card.instance.SetTitle(text);
+	}
+
+	// --- SAVE HANDLING ---
+	public virtual Dictionary Save() {
+		var dict = new Dictionary();
+
+		dict.Add("Value", Text);
+
+		return dict;
+	}
+
+	public virtual void Load(Dictionary data) {
+		Text = (string) data["Value"];
+		OnTextChanged(Text);
+	}
+}
